5914a9486956de3b2db1d5a30bbac86962543839
phyber <phyber@localhost>
2018-07-19 00:50:06 +0100

    Convert tabs to spaces

--- ItemStackSize.lua
+++ ItemStackSize.lua
@@ -1,19 +1,19 @@
 local L_MAXSTACKSIZE
 do
-	local locale = GetLocale()
-	if locale == "enUS" then
-		L_MAXSTACKSIZE = "Max Stack Size: %s"
-	end
+    local locale = GetLocale()
+    if locale == "enUS" then
+        L_MAXSTACKSIZE = "Max Stack Size: %s"
+    end
 end
 local GetItemInfo = GetItemInfo
 local select = select
 local function AddStackSize(tooltip, ...)
-	local name, link = tooltip:GetItem()
-	if link then
-		local maxStack = select(8, GetItemInfo(link))
-		if maxStack then
-			tooltip:AddLine(L_MAXSTACKSIZE:format(maxStack))
-		end
-	end
+    local name, link = tooltip:GetItem()
+    if link then
+        local maxStack = select(8, GetItemInfo(link))
+        if maxStack then
+            tooltip:AddLine(L_MAXSTACKSIZE:format(maxStack))
+        end
+    end
 end
 GameTooltip:HookScript("OnTooltipSetItem", AddStackSize)
